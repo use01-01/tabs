5de43ad71480f76068ee93340f95c85f458e28cd
Madiyar Erm <madiyar.170200@gmail.com>
2021-06-14 13:57:05 +0600

    hi

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
-function App() {
+const url = 'https://course-api.com/react-tabs-project';
+
+const App = () => {
+  const [users, setUsers] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [currentIndex, setCurrentIndex] = useState('');
+  const fetchData = () => {
+    return fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        setUsers(data);
+        setIsLoading(false);
+      })
+      .catch((err) => console.error(err));
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  console.log(currentIndex);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {isLoading && <h2>Loading...</h2>}
+      {users.map((user, index) => {
+        const { company, dates, duties, id, order, title } = user;
+        return (
+          <div>
+            <div>
+              <button onClick={() => setCurrentIndex(index)}>{company}</button>
+            </div>
+            <div>
+              {currentIndex === index && (
+                <div>
+                  <h1>{title}</h1>
+                  {duties.map((duty, index) => {
+                    return <p>{`${index + 1})${duty}`}</p>;
+                  })}
+                </div>
+              )}
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
-}
+};
 
 export default App;
